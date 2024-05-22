bf8155afdb41d21e67fdaa64eaa884a10f8df09e
sarmiento13 <barrientowsarmiento@gmail.com>
2024-05-21 20:15:12 -0500

    ejercicio de 14 y 15 solucionado menos el 13

--- ejercicios_13-15/main.py
+++ ejercicios_13-15/main.py
@@ -8,8 +8,27 @@
 ##- Si operación es 2 calcula A / B. Vigilamos que B no sea 0...
 ##- Si la operación es 3 calculamos la siguiente fórmula: ( A * B ) / 2.5
 
+
+
+
 #**Ejercicio Catorce**
-##Tenemos la pantalla del móvil bloqueada. Partiendo de un `PIN_SECRETO`, intentaremos desbloquear la pantalla. Tenemos hasta 3 intentos. Simula el proceso con Python. En caso de acceder, lanza al usuario `login correcto`. Sino, `llamando ala policía`.
+##Tenemos la pantalla del móvil bloqueada. Partiendo de un `PIN_SECRETO`, intentaremos desbloquear 
+# la pantalla. Tenemos hasta 3 intentos. Simula el proceso con Python. En caso de acceder, lanza al 
+# usuario `login correcto`. Sino, `llamando ala policía`.
+
+pin_secreto:str="2005"
+intentos_restantes:int=3
+while intentos_restantes >0 :
+    pin_usuario:str=input("ingrese un PIN: ")
+    if pin_usuario == pin_secreto:
+        print("login correcto")
+        break
+    else:
+        intentos_restantes -= 1
+        if intentos_restantes >0:
+            print(f"te quedan {intentos_restantes} intentos restantes")
+        else:
+            print("has agotado tus intentos.  Llamando a la policia")
 
 #**Ejercicio Quince**
 ##Crea un algoritmo para la sucesión de Fibonacci. La sucesión de Fibonacci es la siguiente serie:
@@ -17,3 +36,12 @@
 ##`0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89`
 #
 ##Pista: Empezando por 0 y 1, el siguiente número es la suma de los dos números últimos.
+
+a, b = 0, 1
+print(a)
+contador = 1
+n = 10
+while contador < n:
+    a, b = b, a + b
+    print(a)
+    contador += 1
